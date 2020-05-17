8d9685c384476466d79d7e41acab1836dd1f9e64
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-05-17 10:11:11 -0500

    finished TommyShell

--- FigureHub_CLI/FigureHub_CLI/Program.cs
+++ FigureHub_CLI/FigureHub_CLI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
+using TommyShell;
 
 namespace FigureHub_CLI
 {
@@ -18,57 +19,155 @@ namespace FigureHub_CLI
             }
 
             Console.WriteLine("using " + db);
-            SQLiteConnection connection;
+            SQLiteConnection connection = new SQLiteConnection("Data Source=" + db);
 
-            do
+
+            if (!File.Exists(db))
             {
+                //Console.WriteLine(e);
+                Console.WriteLine("DB does not exist. Would you like to create a new DB? (y/N)");
+
+                if (Console.ReadKey().Key != ConsoleKey.Y)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Abort.");
+                    Environment.Exit(1);
+                }
+
+                Console.WriteLine();
+
                 try
                 {
-                    connection = new SQLiteConnection("Data Source=" + db);
+                    if (!Directory.Exists(homeDir))
+                    {
+                        Directory.CreateDirectory(homeDir);
+                    }
+
+                    //connection = new SQLiteConnection("Data Source=" + db);
                     connection.Open();
+
+                    InitDB(connection);
+                    Console.WriteLine("New database will be at " + db);
                 }
-                catch (Exception e)
+                catch (Exception ee)
                 {
-                    connection = null;
-                    //Console.WriteLine(e);
-                    Console.WriteLine("DB does not exist. Would you like to create a new DB? (y/N)");
-
-                    if (Console.ReadKey().Key != ConsoleKey.Y)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Abort.");
-                        Environment.Exit(1);
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                    }
-
-                    try
-                    {
-                        if (!Directory.Exists(homeDir))
-                        {
-                            Directory.CreateDirectory(homeDir);
-                        }
-
-                        SQLiteConnection.CreateFile(db);
-                        InitDB(connection);
-                        Console.WriteLine("New database will be at " + db);
-                    }
-                    catch (Exception ee)
-                    {
-                        Console.WriteLine("ERROR: Cannot create database file.");
-                        Console.WriteLine(e);
-                        Console.WriteLine("Abort.");
-                    }
-
+                    Console.WriteLine("ERROR: Cannot create database file.");
+                    Console.WriteLine(ee);
+                    Console.WriteLine("Abort.");
+                    Environment.Exit(1);
                 }
-
             }
-            while (connection == null);
+            else
+            {
+                //connection = new SQLiteConnection("Data Source=" + db);
+                connection.Open();
+            }
 
-            ManualDebug(connection);
+            //ManualDebug(connection);
 
+            //Menu start = new Menu("Start");
+            //start.AddItem(new Menu.Item("First Item", () => Console.WriteLine("First Item Choose!")));
+            //start.AddItem(new Menu.Item("Second Item", () => Console.WriteLine("Second Item Choose!")));
+            //start.AddItem(new Menu.Item("Go deeper", () =>
+            //{
+            //    Menu deep = new Menu("Deeper");
+            //    deep.AddItem(new Menu.Item("Go deeper.First Item", () => Console.WriteLine("Go deeper.First Item Choose!")));
+            //    deep.AddItem(new Menu.Item("Go deeper.Second Item", () => Console.WriteLine("Go deeper.Second Item Choose!")));
+            //    deep.Start();
+            //}));
+            //start.Start();
+            Menu.DebugAction = () => ManualDebug(connection);
+            Menu start = new Menu("Start");
+            start.AddItem(new Menu.Item("View transactions", () => ShowRecentTransactions()));
+            start.AddItem(new Menu.Item("Add transactions" , () => AddTransactions()));
+            start.AddItem(new Menu.Item("Manage cards"     , () => ManageCards()));
+            start.Start();
+        }
+
+        private static void ManageCards()
+        {
+            Menu manageCards = new Menu("Manage cards");
+            // display cards based on the usage
+            manageCards.AddItem(new Menu.Item("Add a new card", () => AddNewCard()));
+
+            throw new NotImplementedException();
+        }
+
+        private static void AddNewCard()
+        {
+            Menu addcard = new Menu("New Card");
+            addcard.AddItem(new Menu.Item("Credit Card" , () => AddNewCardDetails(1)));
+            addcard.AddItem(new Menu.Item("Debit Card"  , () => AddNewCardDetails(2)));
+            addcard.AddItem(new Menu.Item("Prepaid Card", () => AddNewCardDetails(3)));
+            addcard.AddItem(new Menu.Item("Charge Card" , () => AddNewCardDetails(4)));
+            addcard.Start();
+
+            throw new NotImplementedException();
+        }
+
+
+
+        ////////////////VIEW//////////////////////////
+
+        private static void ShowRecentTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ShowTransactions(int from, int to = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ShowTransactions(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ShowTransactions(DateTime from)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ShowCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        ////////////////Controller//////////////////////////
+
+        private static void AddNewCardDetails(int type)
+        {
+            // 1 for Credit, 2 for Debit, 3 for Prepaid, 4 for Charge
+            throw new NotImplementedException();
+        }
+
+        private static void AddBankAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddTransactions()
+        {
+            // choose card, then fill in details
+            throw new NotImplementedException();
+        }
+
+        private static void ModifyTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ModifyBankAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ModifyCardDetails()
+        {
+            throw new NotImplementedException();
         }
 
         static void ManualDebug(SQLiteConnection connection)
@@ -93,8 +192,7 @@ namespace FigureHub_CLI
                     {
                         Console.WriteLine();
                         connection.Close();
-                        File.Delete(db);
-                        Console.WriteLine("Peace.");
+                        NukeDB();
                         Environment.Exit(0);
                     }
                     else
@@ -128,49 +226,70 @@ namespace FigureHub_CLI
         {
             using var cmd = new SQLiteCommand(connection);
 
-            cmd.CommandText = @"CREATE TABLE Cards 
-            (
-                CardType TEXT,
-                CardNumber TEXT PRIMARY KEY,
-                CardName TEXT,
-                ValidThru DATE,
-                CardholderName TEXT,
-                CVV TEXT,
-                Currency CHAR[3] NOT NULL,
-                Balance REAL,
-                DueDate DATE,
-                DueBalance REAL,
-                Active BOOL NOT NULL
-            );";
-            cmd.ExecuteNonQuery();
+            try
+            {
+                cmd.CommandText = @"CREATE TABLE Cards 
+                (
+                    CardType TEXT,
+                    CardNumber TEXT PRIMARY KEY,
+                    CardName TEXT,
+                    ValidThru DATE,
+                    CardholderName TEXT,
+                    CVV TEXT,
+                    Currency CHAR[3] NOT NULL,
+                    Balance REAL,
+                    DueDate DATE,
+                    DueBalance REAL,
+                    Active BOOL NOT NULL
+                );";
+                cmd.ExecuteNonQuery();
 
-            // rowid PRIMARY KEY
-            cmd.CommandText = @"CREATE TABLE BankAccount 
-            (
-                BankAccountNumber TEXT PRIMARY KEY,
-                Balance REAL,
-                Currency CHAR[3] NOT NULL,
-                Active BOOL NOT NULL,
-                Comments TEXT,
-                LinkedCardNumber FOREIGN KEY REFERENCE Cards(CardNumber)
-            );";
-            cmd.ExecuteNonQuery();
+                // rowid PRIMARY KEY
+                cmd.CommandText = @"CREATE TABLE BankAccount 
+                (
+                    BankAccountNumber TEXT PRIMARY KEY,
+                    Balance REAL,
+                    Currency CHAR[3] NOT NULL,
+                    Active BOOL NOT NULL,
+                    Comments TEXT,
+                    LinkedCardNumber TEXT,
+                    FOREIGN KEY (LinkedCardNumber) REFERENCES Cards(CardNumber)
+                );";
+                cmd.ExecuteNonQuery();
 
-            cmd.CommandText = @"CREATE TABLE Transactions 
-            (
-                Description TEXT,
-                Amount REAL NOT NULL,
-                Currency CHAR[3] NOT NULL,
-                Date DATE,
-                Pending BOOL,
-                ForeignCurrencyAmount REAL,
-                ForeignCurrency CHAR[3],
-                CardNumber FOREIGN KEY REFERENCE Cards(CardNumber),
-                BankAccountNumber FOREIGN KEY REFERENCE BankAccount(BankAccountNumber)
-            );";
-            cmd.ExecuteNonQuery();
+                cmd.CommandText = @"CREATE TABLE Transactions 
+                (
+                    Description TEXT,
+                    Amount REAL NOT NULL,
+                    Currency CHAR[3] NOT NULL,
+                    Date DATE,
+                    Pending BOOL,
+                    ForeignCurrencyAmount REAL,
+                    ForeignCurrency CHAR[3],
+                    CardNumber TEXT,
+                    BankAccountNumber TEXT,
+                    FOREIGN KEY (CardNumber) REFERENCES Cards(CardNumber),
+                    FOREIGN KEY (BankAccountNumber) REFERENCES BankAccount(BankAccountNumber)
+                );";
+                cmd.ExecuteNonQuery();
 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                connection.Close();
+                NukeDB();
+                Environment.Exit(3);
+            }
 
         }
+
+        static void NukeDB()
+        {
+            File.Delete(db);
+            Console.WriteLine("Peace.");
+        }
     }
+
+    
 }
---
+++ FigureHub_CLI/FigureHub_CLI/TommyShell.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TommyShell
+{
+    public class Menu
+    {
+        public string title;
+        public List<Item> items;
+        public static Action DebugAction { get; set; } = null;
+        public static char DebugSwitch { get; set; } = 'D';
+        public static char GoUpSwitch { get; set; } = 'C';
+
+        public Menu(string title)
+        {
+            this.title = title;
+            this.items = new List<Item>();
+        }
+
+        public Menu(string title, List<Item> items)
+        {
+            this.title = title;
+            this.items = items;
+        }
+
+        private Menu() { }
+
+        public void Start(bool newScreen = true)
+        {
+            while (true)  // main loop
+            {
+                if (newScreen)
+                {
+                    Console.Clear();
+                }
+
+                Console.WriteLine(title);
+                foreach (char c in title)
+                {
+                    Console.Write('=');
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                for (int i = 0; i < items.Count; i++)  // display items
+                {
+                    Console.WriteLine("    " + (i + 1).ToString() + ". " + items[i].label);
+                }
+
+                Console.WriteLine();
+
+                while (true)  // interaction loop
+                {
+                    Console.Write($"Choose from 1 to {items.Count}, or {GoUpSwitch} for top: ");
+
+                    char key = char.ToUpper(Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+
+                    if (key == GoUpSwitch)
+                    {
+                        return; // to previous menu loop
+                    }
+                    else if (key == DebugSwitch)
+                    {
+                        DebugAction?.Invoke();
+                    }
+                    else
+                    {
+                        int i = (int)char.GetNumericValue(key);
+                        if (i == -1 || i > items.Count)
+                        {
+                            Console.WriteLine("Invalid answer.");
+                            continue;
+                        }
+                        else
+                        {
+                            items[i - 1].Action();
+
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AddItem(Item toAdd)
+        {
+            items.Add(toAdd);
+        }
+
+        public class Item
+        {
+            public string label;
+            public Action action;
+
+            public Item(string label, Action action)
+            {
+                this.label = label;
+                this.action = action;
+            }
+
+            public void Action()
+            {
+                action?.Invoke();
+            }
+        }
+    }
+}
